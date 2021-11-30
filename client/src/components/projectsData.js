15c7ddbbc46b9c707654a8c35609edb5215032b1
Richard Medina <medinarichard996@gmail.com>
2021-11-30 03:02:40 -0800

    Fixing text

--- client/src/components/projectsData.js
+++ client/src/components/projectsData.js
@@ -1,18 +1,18 @@
 module.exports = [{
   title: 'Leaft',
-  description: 'MERN stack application allowing users to create and select rides to locations they would like to go. I integrated authentication utilizing Passport.js. The routes I created in the back end handled users registeration and login. Once logged in I set up a session, enabling a user who is authentciated to have access to the proteced routes.',
+  description: 'MERN stack application allowing users to create and select rides to locations they would like to go. I integrated authentication utilizing Passport.js. The routes I created in the back end handled users registeration and login. Once logged in I set up a session, enabling a user who is authentciated to have access to the protected routes.',
   link: "https://giphy.com/embed/9q82ejfdhkKPrB0D4Y",
   href: "https://youtu.be/xDD9C5PmWu4"
 },
 {
   title: 'Atelier',
-  description: 'A team of three others made a shopping website incorparting the MERN stack. For my portion of the application, I handled all things reviews.',
+  description: 'A team of three others made a shopping website incorporating the MERN stack. For my portion of the application, I handled all things reviews.',
   link: "https://giphy.com/embed/KKszE18coUdK33siGZ",
   href: "https://youtu.be/KGn3wilj0vE",
 },
 {
   title: 'Bill Splitter',
-  description: 'Built a tip calculaor using React. I handled error and display apporiate error messages. Users can select different tip amounts shown.',
+  description: 'Built a tip calculator using React. I handled errors and displayed apporiate error messages. Users can select different tip amounts shown.',
   link: "https://giphy.com/embed/PZL01em7ozrOBdM1VA",
   href: "https://youtu.be/Q3HQR5YDjnw"
 }]
